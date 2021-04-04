fbaeb33082a0e87f95925678d707e98350b3cafa
sergey <nogabomzha228@mail.ru>
2021-04-04 06:52:44 -0700

    fixes

--- orange/views.py
+++ orange/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import render
 from .models import Title
 from django.core.exceptions import ObjectDoesNotExist
 
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
 def index(request):
     if request.method == "POST":
+       #logger sret massages v console
+        logger.info('Post received!')
+
         title = request.POST["title"]
+        logger.info(title)
+
         try:
             result = Title.objects.get(name=title)
+            logger.info(result)
             
         except ObjectDoesNotExist:
             result = "Not found"
-        
-            return render(request, 'orange/index.html', {'retitle': result})
+            logger.info(result)
         
         return render(request, 'orange/index.html', {'retitle': result})
 
